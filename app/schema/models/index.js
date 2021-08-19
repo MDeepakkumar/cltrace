2d75740b8bc527bd10ab911a1ac9d1f5cae11177
MDeepakkumar <89176262+MDeepakkumar@users.noreply.github.com>
2021-08-19 09:25:02 +0530

    Create index.js

---
+++ app/schema/models/index.js
@@ -0,0 +1,13 @@
+import { users } from "./users.js";
+import { traceTickets } from "./traceTickets.js";
+import { traceTicketComments } from "./traceTicketComment.js";
+import { traceUserGroups } from "./traceUserGroup.js";
+import { traceUserGroupMappings } from "./traceUserGroupMapping.js";
+
+export {
+  users,
+  traceTickets,
+  traceUserGroupMappings,
+  traceUserGroups,
+  traceTicketComments,
+};
